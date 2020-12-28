655033fc6600c04043d00bbaa01e80cc10e240a5
Rafael Antunes <rafaelmessias710@gmail.com>
2020-12-28 13:43:00 -0400

    Close server afterAll

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -10,6 +10,4 @@ beforeAll(async () => {
   global.testRequest = supertest(server.getApp());
 });
 
-afterAll(async () => {
-  await server.close();
-});
+afterAll(async () => await server.close());
