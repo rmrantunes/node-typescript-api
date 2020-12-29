5e3b411d66516a4d5b66151f5aba14c454a7a554
Rafael Antunes <rafaelmessias710@gmail.com>
2020-12-29 17:23:29 -0400

    C05P03. Testing if a user is created with a encrypted password

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,19 +1,25 @@
 import { User } from "@src/models/user";
+import AuthService from "@src/services/auth";
 
 describe("users functional test", () => {
   beforeEach(() => User.deleteMany({}));
   describe("when creating a new user", () => {
-    it("should successfully create a new user", async () => {
+    it("should successfully create a new user with encrypted password", async () => {
       const newUser = {
         name: "John Doe",
         email: "john@test.com",
         password: "1234",
       };
-
       const response = await global.testRequest.post("/users").send(newUser);
       expect(response.status).toBe(201);
-      expect(response.body).toEqual(expect.objectContaining(newUser));
+      await expect(
+        AuthService.comparePasswords(newUser.password, response.body.password),
+      ).resolves.toBeTruthy();
+      expect(response.body).toEqual(
+        expect.objectContaining({ ...newUser, password: expect.any(String) }),
+      );
     });
+
     it("should return 422 when there is a validation error", async () => {
       const newUser = {
         email: "john@test.com",
