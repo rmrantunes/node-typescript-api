f416e1c5ce1f3ec6639efe4b7c5366580f0d5a84
Rafael Antunes <rafaelmessias710@gmail.com>
2020-12-30 18:57:41 -0400

    C05P05. Authorization Middleware implementation in Forecast Controller

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from "@overnightjs/core";
+import { ClassMiddleware, Controller, Get } from "@overnightjs/core";
+import { authMiddleware } from "@src/middlewares/auth";
 import { Beach } from "@src/models/beach";
 import { Forecast } from "@src/services/forecast";
 import { Request, Response } from "express";
@@ -7,14 +8,15 @@ const forecast = new Forecast();
 // Automaticamente vai ter uma rota na api chamada "/forecast"
 // com esse controller
 @Controller("forecast")
+@ClassMiddleware(authMiddleware)
 export class ForecastController {
   @Get("")
   public async getForecastForLoggedUser(
-    _: Request,
+    req: Request,
     res: Response,
   ): Promise<void> {
     try {
-      const beaches = await Beach.find({});
+      const beaches = await Beach.find({user: req.decoded?.id});
       const forecastData = await forecast.processForecastForBeaches(beaches);
 
       res.status(200).send(forecastData);
