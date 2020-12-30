7fcddc552dd51cee5ca5017395f0a305c288edb6
Rafael Antunes <rafaelmessias710@gmail.com>
2020-12-30 18:54:27 -0400

    C05P05. decodeToken static method & generate payload type changed

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,7 +2,11 @@ import config from "config";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { LeanDocument } from "mongoose";
-import { UserModel } from "@src/models/user";
+import { User, UserModel } from "@src/models/user";
+
+export interface DecodedUser extends Omit<User, "_id"> {
+  id: string;
+}
 
 export default class AuthService {
   public static async hashPassword(
@@ -19,9 +23,15 @@ export default class AuthService {
     return await bcrypt.compare(password, hashedPassword);
   }
 
-  public static generateToken(payload: LeanDocument<UserModel>): string {
+  public static generateToken(
+    payload: LeanDocument<UserModel> | Record<string, unknown>,
+  ): string {
     return jwt.sign(payload, config.get("App.auth.key"), {
       expiresIn: config.get("App.auth.tokenExpiresIn"),
     });
+  }
+
+  public static decodeToken(token: string): DecodedUser {
+    return jwt.verify(token, config.get("App.auth.key")) as DecodedUser;
   }
 }
