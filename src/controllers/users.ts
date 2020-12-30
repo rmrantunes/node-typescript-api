d3f495318dff0cb9279d79d7f7a4584d06232ac1
Rafael Antunes <rafaelmessias710@gmail.com>
2020-12-30 10:53:34 -0400

    C05P04. User authentication route added and working

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,11 +1,12 @@
 import { Controller, Post } from "@overnightjs/core";
 import { User } from "@src/models/user";
+import AuthService from "@src/services/auth";
 import { Request, Response } from "express";
 import { BaseController } from ".";
 
 @Controller("users")
 export class UsersController extends BaseController {
-  @Post("")
+  @Post()
   public async create(req: Request, res: Response): Promise<void> {
     try {
       const user = new User(req.body);
@@ -15,5 +16,30 @@ export class UsersController extends BaseController {
     } catch (error) {
       this.sendCreateUpdateErrorResponse(res, error);
     }
+  }
+
+  @Post("authenticate")
+  public async authenticate(
+    req: Request,
+    res: Response,
+  ): Promise<Response | undefined> {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).send({
+        code: 401,
+        error: "User not found",
+      });
+    }
+    if (!(await AuthService.comparePasswords(password, user.password))) {
+      return res.status(401).send({
+        code: 401,
+        error: "Wrong password",
+      });
+    }
+    const token = AuthService.generateToken(user.toJSON());
+
+    return res.status(200).send({ token });
   }
 }
