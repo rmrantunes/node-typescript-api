7de67aaba7183232e069348325fcfc0309f5318f
Rafael Antunes <rafaelmessias710@gmail.com>
2020-12-30 18:57:08 -0400

    C05P05. User addition in beachSchema

--- src/models/beach.ts
+++ src/models/beach.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Model } from "mongoose";
+import mongoose, { Schema, Document, Model } from "mongoose";
 
 export enum BeachPosition {
   N = "N",
@@ -12,6 +12,7 @@ export interface Beach {
   lat: number;
   lng: number;
   name: string;
+  user: string;
   position: BeachPosition;
 }
 
@@ -21,6 +22,7 @@ const beachSchema = new mongoose.Schema(
     lng: { type: Number, required: true },
     name: { type: String, required: true },
     position: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   },
   {
     toJSON: {
