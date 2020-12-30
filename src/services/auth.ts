c6c2295bfee06afe4a70c60ed99ff189294c90db
Rafael Antunes <rafaelmessias710@gmail.com>
2020-12-30 10:55:47 -0400

    C05P04. generateToken static AuthService method.

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,8 @@
+import config from "config";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { LeanDocument } from "mongoose";
+import { UserModel } from "@src/models/user";
 
 export default class AuthService {
   public static async hashPassword(
@@ -13,5 +17,11 @@ export default class AuthService {
     hashedPassword: string,
   ): Promise<boolean> {
     return await bcrypt.compare(password, hashedPassword);
+  }
+
+  public static generateToken(payload: LeanDocument<UserModel>): string {
+    return jwt.sign(payload, config.get("App.auth.key"), {
+      expiresIn: config.get("App.auth.tokenExpiresIn"),
+    });
   }
 }
