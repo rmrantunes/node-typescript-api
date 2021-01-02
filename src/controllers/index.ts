d05dd177f3da8ce4c17b2b98308056454eaa80f6
Rafael Antunes <rafaelmessias710@gmail.com>
2021-01-02 10:30:42 -0400

    C07P03. ApiError Class implementation

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,6 @@
 import logger from "@src/logger";
 import { CUSTOM_VALIDATION } from "@src/models/user";
+import ApiError, { APIError } from "@src/util/errors/api-error";
 import { Response } from "express";
 import mongoose from "mongoose";
 
@@ -10,10 +11,19 @@ export abstract class BaseController {
   ): void {
     if (error instanceof mongoose.Error.ValidationError) {
       const clientErrors = this.handleClientErrors(error);
-      res.status(clientErrors.code).send(clientErrors);
+      res
+        .status(clientErrors.code)
+        .send(
+          ApiError.format({
+            code: clientErrors.code,
+            message: clientErrors.error,
+          }),
+        );
     } else {
-      logger.error(error)
-      res.status(500).send({ code: 500, error: "Something went wrong" });
+      logger.error(error);
+      res
+        .status(500)
+        .send(ApiError.format({ code: 500, message: "Something went wrong" }));
     }
   }
 
@@ -28,5 +38,9 @@ export abstract class BaseController {
     } else {
       return { code: 422, error: error.message };
     }
+  }
+
+  protected sendErrorResponse(res: Response, apiError: APIError): Response {
+    return res.status(apiError.code).send(ApiError.format(apiError));
   }
 }
