d35b87a956f91038cd408fa896abe880841732b1
Rafael Antunes <rafaelmessias710@gmail.com>
2021-01-05 07:48:21 -0400

    C09P01. OpenAPIValidator and swaggerUi implementation

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import bodyParser from "body-parser";
 import { Application } from "express";
 import expressPino from "express-pino-logger";
 import cors from "cors";
+import apiSchema from "@src/api.schema.json";
+import swaggerUi from "swagger-ui-express";
+import { OpenApiValidator } from "express-openapi-validator";
+import { OpenAPIV3 } from "express-openapi-validator/dist/framework/types";
 import { ForecastController } from "./controllers/forecast";
 import * as database from "@src/database";
 import { BeachesController } from "./controllers/beaches";
@@ -17,6 +21,7 @@ export class SetupServer extends Server {
 
   public async init(): Promise<void> {
     this.setupExpress();
+    await this.docsSetup();
     this.setupControllers();
     await this.setupDatabase();
   }
@@ -58,5 +63,14 @@ export class SetupServer extends Server {
 
   public getApp(): Application {
     return this.app;
+  }
+
+  private async docsSetup(): Promise<void> {
+    this.app.use("/docs", swaggerUi.serve, swaggerUi.setup(apiSchema));
+    await new OpenApiValidator({
+      apiSpec: apiSchema as OpenAPIV3.Document,
+      validateRequests: true,
+      validateResponses: true,
+    }).install(this.app);
   }
 }
