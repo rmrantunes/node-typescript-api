7585e3a9fcada2eb2d2d26b833c817d10d4b04cb
Rafael Antunes <rafaelmessias710@gmail.com>
2021-01-05 08:14:25 -0400

    C09P02. API Error Validator Middleware implementation

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ import * as database from "@src/database";
 import { BeachesController } from "./controllers/beaches";
 import { UsersController } from "./controllers/users";
 import logger from "./logger";
+import { apiErrorValidator } from "./middlewares/api-error-validator";
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -24,6 +25,7 @@ export class SetupServer extends Server {
     await this.docsSetup();
     this.setupControllers();
     await this.setupDatabase();
+    this.setupErrorHandlers();
   }
 
   private setupExpress(): void {
@@ -34,6 +36,10 @@ export class SetupServer extends Server {
         origin: "*",
       }),
     );
+  }
+
+  private setupErrorHandlers(): void {
+    this.app.use(apiErrorValidator);
   }
 
   private setupControllers(): void {
