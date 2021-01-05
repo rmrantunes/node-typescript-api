2fe55d0e3f9226de4b98181785b22d7356bc7e39
Rafael Antunes <rafaelmessias710@gmail.com>
2021-01-05 07:49:27 -0400

    C09P01. Due the api docs, error 422 is no more, now it is 400

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -20,17 +20,17 @@ describe("users functional test", () => {
       );
     });
 
-    it("should return 422 when there is a validation error", async () => {
+    it("should return 400 when a field is missing", async () => {
       const newUser = {
         email: "john@test.com",
         password: "1234",
       };
 
       const response = await global.testRequest.post("/users").send(newUser);
-      expect(response.status).toBe(422);
+      expect(response.status).toBe(400);
       expect(response.body).toEqual({
-        code: 422,
-        error: "Unprocessable Entity",
+        code: 400,
+        error: "Bad Request",
         message: "User validation failed: name: Path `name` is required.",
       });
     });
