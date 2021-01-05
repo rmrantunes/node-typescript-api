a82ebb5fe774b7939e062b9817bd15457a99f9c6
Rafael Antunes <rafaelmessias710@gmail.com>
2021-01-05 10:29:04 -0400

    C09P03. Rate Limit implementation as /forecast middleware

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,18 +1,42 @@
-import { ClassMiddleware, Controller, Get } from "@overnightjs/core";
+import {
+  ClassMiddleware,
+  Controller,
+  Get,
+  Middleware,
+} from "@overnightjs/core";
 import { authMiddleware } from "@src/middlewares/auth";
 import { Beach } from "@src/models/beach";
 import { Forecast } from "@src/services/forecast";
-import { Response } from "express";
+import { Response, Request as ExpressRequest } from "express";
 import { Request } from "@src/types";
 import { BaseController } from ".";
+import rateLimit from "express-rate-limit";
+import ApiError from "@src/util/errors/api-error";
 
 const forecast = new Forecast();
+
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 10,
+  keyGenerator(req: ExpressRequest): string {
+    return req.ip;
+  },
+  handler(_, res: Response): void {
+    res.status(429).send(
+      ApiError.format({
+        code: 429,
+        message: "Too many request to the /forecast endpoint",
+      }),
+    );
+  },
+});
 // Automaticamente vai ter uma rota na api chamada "/forecast"
 // com esse controller
 @Controller("forecast")
 @ClassMiddleware(authMiddleware)
 export class ForecastController extends BaseController {
   @Get("")
+  @Middleware(rateLimiter)
   public async getForecastForLoggedUser(
     req: Request,
     res: Response,
@@ -23,7 +47,10 @@ export class ForecastController extends BaseController {
 
       res.status(200).send(forecastData);
     } catch (error) {
-      this.sendErrorResponse(res, { code: 500, message: "Something went wrong" });
+      this.sendErrorResponse(res, {
+        code: 500,
+        message: "Something went wrong",
+      });
     }
   }
 }
